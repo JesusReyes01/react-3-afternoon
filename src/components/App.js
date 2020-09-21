d8ee7eff7ca4d6fd2b40689d87cd396c0adcc5a6
jesusreyes01 <jesusareyes01@gmail.com>
2020-09-21 18:42:56 -0500

    main project finished. have not tackled black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
@@ -19,19 +21,38 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get(`https://practiceapi.devmountain.com/api/posts`)
+    .then(res => {
+      // console.log(res)
+      this.setState({posts: res.data})
 
+    })
+    .catch(err => console.log(err))
   }
-
-  updatePost() {
+  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })//finds id number then replaces text with new text.
+    .then(res => {
+      this.setState({ posts: res.data})//it then assigns new data to this.state.posts
+    })
+    .catch(err => console.log(err))
   
   }
-
-  deletePost() {
-
+ 
+  deletePost( id ) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(res => {
+      this.setState({posts: res.data})
+    })
+    .catch(err => console.log(err))
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', {text})
+    .then( res => {
+      this.setState({posts: res.data})
+    })
+    .catch(err => console.log(err))
   }
 
   render() {
@@ -43,7 +64,17 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={this.createPost} />
+          {
+            posts.map( e => (
+              <Post key={ e.id } 
+                    text={ e.text }
+                    date={ e.date }
+                    id={ e.id }
+                    updatePostFn={this.updatePost}
+                    deletePostFn={this.deletePost}/>
+            ))
+          }
           
         </section>
       </div>
